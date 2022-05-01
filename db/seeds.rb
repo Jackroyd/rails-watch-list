bab4702e4f1742967158386b22780e94afcaa14a
Josh Ackroyd <ackroydjosh93@gmail.com>
2022-05-01 17:54:00 +0100

    saving and pushiong work

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+
+puts 'Deleting current Movies'
+5.times do
+  print('.')
+  sleep(0.2)
+end
+Movie.destroy_all
+puts 'Movies destroyed'
+
+150.times do
+  title = "#{Faker::Movies::HarryPotter.character} and the #{Faker::Verb.past_participle} #{Faker::TvShows::TheThickOfIt.position}"
+  overview = Faker::TvShows::SouthPark.quote
+  poster_url = Faker::Fillmurray.image
+  rating = rand(0.0..10.0)
+  Movie.create(title: title, overview: overview, poster_url: poster_url, rating: rating)
+  puts("Movie '#{title}' created")
+end
+
+puts 'Completed'
